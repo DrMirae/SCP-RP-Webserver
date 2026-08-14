bbf4856821c06c3c1cff949958005f3464b8bebe
Mirae <tessa.nexein@gmail.com>
2026-08-14 13:39:32 +0200

    Comment out unused JSON file reference in config.

--- config/index.js
+++ config/index.js
@@ -8,8 +8,9 @@ export const SERVER_PORT = process.env.SERVER_PORT || 3413;
 
 assert(SERVER_PORT, 'SERVER_PORT must be defined in .env')
 
-const spheres_json = fs.readFileSync('./objects/spheres.json', 'utf8');
-export const spheres = JSON.parse(spheres_json);
+// Replace with other object data
+//const spheres_json = fs.readFileSync('./objects/spheres.json', 'utf8');
+//export const spheres = JSON.parse(spheres_json);
 
 export const DB_HOST = process.env.DB_HOST;
 export const DB_USER = process.env.DB_USER;
